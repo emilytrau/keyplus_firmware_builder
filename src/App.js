c3fb7dbbe784ecc6e4df7d39fbb13a87ef243348
Angus Trau <angus.amtra5@gmail.com>
2017-09-28 19:51:43 +1000

    Added app bar and drawer

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BuilderAppBar from './components/BuilderAppBar.js';
+import './App.css';
+
+import 'typeface-roboto';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <BuilderAppBar />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/BuilderAppBar.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import Drawer from 'material-ui/Drawer';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import FileUploadIcon from 'material-ui-icons/FileUpload';
+import ContentPasteIcon from 'material-ui-icons/ContentPaste';
+import SvgIcon from 'material-ui/SvgIcon';
+
+const styles = theme => ({
+    root: {
+        width: '100%'
+    },
+    flex: {
+        flex: 1
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+    },
+    drawerList: {
+        width: 250,
+        flex: 'initial'
+    }
+});
+
+class BuilderAppBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isDrawerOpen: false
+        }
+
+        this.toggle = this.toggleDrawer.bind(this);
+        this.useConfig = this.useConfig.bind(this);
+        this.useKLE = this.useKLE.bind(this);
+    }
+
+    // Sets the drawer open state to 'state' or toggle if not provided
+    toggleDrawer(state) {
+        this.setState({ isDrawerOpen: state || !this.state.isDrawerOpen });
+    }
+
+    // Load existing config
+    useConfig() {
+
+    }
+
+    // Load new config from KLE layout
+    useKLE() {
+
+    }
+
+    render() {
+        const classes = this.props.classes;
+
+        return (
+            <div className={ classes.root }>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <IconButton className={ classes.menuButton } color='contrast' aria-label='menu' onClick={ () => this.toggleDrawer(true) }>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography type='title' color='inherit' className={ classes.flex }>
+                            keyplus Firmware Builder
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer open={ this.state.isDrawerOpen } onRequestClose={ () => this.toggleDrawer(false) }>
+                    <div tabIndex={ 0 } role='button' onClick={ () => this.toggleDrawer(false) }>
+                        <div>
+                            <List className={ classes.drawerList }>
+                                <ListItem button onClick={ this.useConfig }>
+                                    <ListItemIcon>
+                                        <FileUploadIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary='Upload config' />
+                                </ListItem>
+                                <ListItem button onClick={ this.useKLE }>
+                                    <ListItemIcon>
+                                        <ContentPasteIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary='Upload KLE from clipboard' />
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem button component='a' href='https://github.com/angustrau/keyplus_firmware_builder'>
+
+                                </ListItem>
+                            </List>
+                        </div>
+                    </div>
+                </Drawer>
+            </div>
+        );
+    }
+}
+
+BuilderAppBar.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(BuilderAppBar);
